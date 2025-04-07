1f3ecf2950f80a092427623d9ab99faf7724d326
Joe Sema <joesema@Joes-Laptop.local>
2025-04-07 00:18:29 -0400

    searching

--- algorithms/searching.py
+++ algorithms/searching.py
@@ -5,8 +5,25 @@ def binary_search_left_most(nums, target):
     right = n - 1
     ans = -1
 
-    
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] >= target:
+            if nums[mid] == target: ans = mid
+            right = mid - 1
+        else:
+            left = mid + 1
 
+    return ans
+
+def binary_search_rec(nums, target, left, right):
+    while left <= right:
+        mid = (left + right) / 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] > target:
+            return binary_search_rec(nums, target, left, mid - 1)
+        else:
+            return binary_search_rec(nums, target, mid + 1, right)
 
 def binary_search(nums, target):
     n = len(nums)
